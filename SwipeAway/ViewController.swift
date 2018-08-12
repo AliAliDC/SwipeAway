c6acb239cd16c7dfa52b5fe768405387ac23fc27
Ali Ali <AliAAliDC@gmail.com>
2018-08-12 10:07:09 -0400

    Initial Commit

---
+++ SwipeAway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeAway
+//
+//  Created by Ali Ali on 8/12/18.
+//  Copyright © 2018 Ali Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
